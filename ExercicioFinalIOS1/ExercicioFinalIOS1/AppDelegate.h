e70ad65940776b6dccbc96e78bca3950bcf06f37
ALUNO <aluno@hc5mac07.local>
2016-08-31 20:16:39 -0300

    Criando Projeto

---
+++ ExercicioFinalIOS1/ExercicioFinalIOS1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExercicioFinalIOS1
+//
+//  Created by ALUNO on 31/08/16.
+//  Copyright © 2016 Weuller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
