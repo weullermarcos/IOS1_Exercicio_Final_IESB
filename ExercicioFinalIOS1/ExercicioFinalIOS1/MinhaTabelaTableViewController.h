595fcd9f158c6d599752138c66c8bc998a4d7eb3
Hoff Silva <hoffsilva@macbook-pro-de-hoff.local>
2016-09-13 13:18:39 -0300

    Adicionado e removendo itens da lista.

---
+++ ExercicioFinalIOS1/ExercicioFinalIOS1/MinhaTabelaTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  MinhaTabelaTableViewController.h
+//  ExercicioFinalIOS1
+//
+//  Created by Hoff Silva on 13/09/16.
+//  Copyright © 2016 Weuller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MinhaTabelaTableViewController : UITableViewController
+
+@property NSMutableArray *names;
+
+@end
